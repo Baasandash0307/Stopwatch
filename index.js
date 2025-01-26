222128ed355fd589825441b4fdf279f9e0773b7a
Baasandash0307 <bsskaa.kai123@gmail.com>
2025-01-26 23:19:02 +0800

    new

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
- // const tipsRef = document.getElementById('perc');
-// const tips = document.getElementsByClassName('tip-buttons');
-
-// for (let i = 0; i < tips.length; i++){
-//     tips[i].addEventListener('click', (event) => {
-//         const tipStrings = event.target.innerHTML;
-//         const tipsGetValues = parseFloat(tipStrings.replace('%', ''));
-//         const input = document.getElementById('billAmount')
-//         const inputValue = parseFloat(input.value);
-        
-//         if(!isNaN(inputValue)) {
-//             const result = (inputValue * tipsGetValues) / 100;
-//             const sum = result + inputValue;
-//             document.getElementById('tip').innerHTML = sum;  
-//         }
-//         else{
-//             console.log('error');
-//         }
-//     })
-// };
-    
-//     document.getElementById('clear').addEventListener('click', () => {
-//     document.getElementById('billAmount').value = '';
-//     document.getElementById('tip').textContent = '';
-// });
-
-
 const countRef = document.getElementById('time');
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
 
-const startRef = document.getElementById('start');
-const stopRef = document.getElementById('stop');
-const resettRef = document.getElementById('reset');
+function start(){
+    if(!isRunning)
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval(update, 10);
+        isRunning = true;
+};
 
+function stop() {
+    if(isRunning) {
+        clearInterval(timer);
+        elapsedTime = Date.now() - startTime;
+        isRunning = false;  
+    }
+};
 
+function reset(){
+    if(isRunning)
+        clearInterval(timer);
+        startTime = 0;
+        elapsedTime = 0;
+        isRunning = false;
+        countRef.textContent = "00:00:00"
+}
+    
+function update(){
+    const currentTime = Date.now();
+    elapsedTime = currentTime - startTime; 
+
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / 1000 % 60);
+
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+
+    countRef.textContent = `${hours}:${minutes}:${seconds}`
+};
+
+document.getElementById('start').addEventListener('click', start);
+document.getElementById('stop').addEventListener('click', stop);
+document.getElementById('reset').addEventListener('click', reset);
